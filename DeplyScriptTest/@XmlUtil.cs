5c78b6f3978c9cef0a33416d1862cd31a431b6a7
shayan <shayan@geeks.ltd.uk>
2013-05-13 16:52:42 +0100

    Add test for settings section (web.config)

---
+++ DeplyScriptTest/@XmlUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace DeplyScriptTest
+{
+    /// <summary>
+    /// Provides a set of utilities to read xml/config files (in particular web.config)
+    /// </summary>
+    static class XmlUtil
+    {
+        public static string ReadSetting(string xmlFilePath, string key)
+        {
+            var doc = XDocument.Load(xmlFilePath);
+            var node = doc.XPathSelectElements(String.Format("/configuration/appSettings/add[@key='{0}']", key)).First();
+            return node.Attribute("value").Value;
+        }
+    }
+}
--- DeplyScriptTest/WebConfigTest.cs
+++ DeplyScriptTest/WebConfigTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeployScript;
 
 namespace DeplyScriptTest
 {
@@ -7,8 +8,18 @@ namespace DeplyScriptTest
     public class WebConfigTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Change_setting()
         {
+            var webConfig = Prepare.WebConfig();
+            var runner = new Runner(new Variables());
+
+            runner.ExecScript("DummyFileName", @"
+[settings]
+Email.Enable.Ssl = False
+");
+            var actualValue = XmlUtil.ReadSetting(webConfig, "Email.Enable.Ssl");
+
+            Assert.AreEqual("False", actualValue);
         }
     }
 }
