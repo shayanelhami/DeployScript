6fc657a4308416c859397f3f31a371a41f688d6d
shayan <shayan@geeks.ltd.uk>
2013-06-17 14:12:08 +0100

    Add test for compilation section

---
+++ DeplyScriptTest/CompilationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeployScript;
+
+namespace DeplyScriptTest
+{
+    [TestClass]
+    public class CompilationTest
+    {
+        [TestMethod]
+        public void Change_setting()
+        {
+            var webConfig = Prepare.WebConfig();
+            var runner = new Runner(new Variables());
+
+            runner.ExecScript("DummyFileName", @"
+[compilation]
+debug = false
+");
+            var actualValue = XmlUtil.ReadAttribute(webConfig, "/configuration/system.web/compilation", "debug");
+
+            Assert.AreEqual("false", actualValue);
+        }
+    }
+}
